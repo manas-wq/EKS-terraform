a6d68ea6772ff14e2cf2ddc4d7f61838795666ec
manas-wq <manas.mohapatra.r@gmail.com>
2026-04-09 16:56:13 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+### check trraform version
+provider "aws" {
+  region = var.aws_region
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
